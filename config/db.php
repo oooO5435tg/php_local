94671291a3a8c65ea2d180e24afcd23de30fa617
oooO5435tg <146661206+oooO5435tg@users.noreply.github.com>
2024-04-01 08:58:11 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'mvc',
-    'username' => 'root',
-    'password' => '',
+    'database' => 'aakirmnp_m2',
+    'username' => 'aakirmnp',
+    'password' => 'LBbSAv',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
